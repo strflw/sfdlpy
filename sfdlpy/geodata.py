94eb3ba08a8734ebfa8f29106b1799c97fc09332
strflw <strflw@pm.me>
2020-01-01 23:39:30 +0100

    Load geodata if not existent

--- sfdlpy/geodata.py
+++ sfdlpy/geodata.py
@@ -1,9 +1,27 @@
+import os
+import urllib.request
+import tarfile
+
 from pathlib import Path
 
 import geoip2.database as geodata
 
-data_path = Path('./data/geodata.db').resolve()
-reader = geodata.Reader(str(data_path))
+GEODATA_PATH = Path('./data/geodata.mmdb').resolve()
+GEODATA_TAR_PATH = Path('./data/geodata.tar.gz').resolve()
+GEODATA_URL_PATH = Path('./data/geodataurl.txt').resolve()
+
+try:
+    reader = geodata.Reader(str(GEODATA_PATH))
+except FileNotFoundError:
+    with GEODATA_URL_PATH.open() as f:
+        urllib.request.urlretrieve(f.readline().strip(), GEODATA_TAR_PATH)
+        for trinfo in (tr:=tarfile.open(GEODATA_TAR_PATH)).getmembers():
+            if trinfo.name.endswith('.mmdb'):
+                tr._extract_member(trinfo, targetpath=str(GEODATA_PATH))
+        tr.close()
+        os.remove(GEODATA_TAR_PATH)
+
+        reader = geodata.Reader(str(GEODATA_PATH))
 
 
 def get_iso_code(ip):
